2a4d8aa6c320a902082e76a89344f89c19e28009
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-07-16 14:38:20 +0800

    feat(context): capture Git diff provenance

--- lua/gator/context.lua
+++ lua/gator/context.lua
@@ -2,6 +2,7 @@ local M = {
 	name = "context",
 	api_version = 1,
 	diagnostic = require("gator.context.diagnostic"),
+	diff = require("gator.context.diff"),
 	file = require("gator.context.file"),
 	pack = require("gator.context.pack"),
 }
