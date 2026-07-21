38696700212cedc1fc16f5cd67c08f1aa5897843
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-07-22 01:10:43 +0800

    feat(ui): add session picker for #332

--- lua/gator/ui.lua
+++ lua/gator/ui.lua
@@ -15,6 +15,7 @@ local M = {
 	picker = require("gator.ui.picker"),
 	provider_picker = require("gator.ui.provider_picker"),
 	selection = require("gator.ui.selection"),
+	session_picker = require("gator.ui.session_picker"),
 	task_file_picker = require("gator.ui.task_file_picker"),
 	sidebar = require("gator.ui.sidebar"),
 	timeline = require("gator.ui.timeline"),
