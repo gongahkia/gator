30eb6c49fd9506b5e9daf301bf48741d3cb1b61e
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-07-21 16:06:44 +0800

    feat(runtime): enforce run policy hooks for #272

--- lua/gator/core.lua
+++ lua/gator/core.lua
@@ -22,6 +22,7 @@ local M = {
 	retention = require("gator.core.retention"),
 	runtime = require("gator.core.runtime"),
 	run = require("gator.core.run"),
+	run_limits = require("gator.core.run_limits"),
 	run_reconciliation = require("gator.core.run_reconciliation"),
 	stream_reconnect = require("gator.core.stream_reconnect"),
 	supervisor = require("gator.core.supervisor"),
